d4335d75d5f541a26713533844bd2eedb96de22b
omsinngle <omsingle982@gmail.com>
2026-08-13 09:25:07 +0000

    Add SSH access to EC2

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
