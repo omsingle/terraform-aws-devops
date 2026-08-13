ed27c0dda9543d9bb0adf2308a3f74b3bffbdfc9
omsinngle <omsingle982@gmail.com>
2026-08-13 09:40:13 +0000

    Automate nginx installation with Terraform

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -33,11 +33,19 @@ resource "aws_security_group" "web" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = "aws_login"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.web.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    dnf update -y
+    dnf install -y nginx
+    systemctl enable nginx
+    systemctl start nginx
+  EOF
 
   tags = {
     Name = "terraform-project3-${var.environment}-web"
